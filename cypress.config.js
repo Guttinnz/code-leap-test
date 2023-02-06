74e07120a01b7cfc538b9d1c9a52f6d9f96ea6d6
Gustavo Bueno <gustavoriedel2202@gmail.com>
2023-02-06 14:44:50 -0300

    add workflow in cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "8zt84v",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
